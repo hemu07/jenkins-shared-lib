a0fd3a7d8a91142d80a9d18b2fc1bda4163aea5e
hemu7 <hemalijayswal07@gmail.com>
2024-04-25 23:31:11 +0530

    added imageName as input parameter

--- buildImage.groovy
+++ buildImage.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-def call(){
+def call(String $imageName){
     echo "building the docker image.."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-creds', passwordVariable: 'PASS', usernameVariable: 'USER')]){
-    sh 'docker build -t hemu07/hemali_repo:jma-5.0 .'
+    sh "docker build -t $imageName ."
     sh "echo $PASS | docker login -u $USER --password-stdin"
-    sh 'docker push hemu07/hemali_repo:jma-5.0'
+    sh "docker push $imageName"
     }
 }
